1522cfc9fee80a4e1b99edfc2363825163805a54
3RMc <ermakoff1994@mail.ru>
2018-02-09 07:51:14 +0300

    Убраны лишние таски

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,5 @@
 var gulp = require('gulp'),
-    browsersync = require('browser-sync'),
-    stylus = require('gulp-stylus');
-
-gulp.task('stylus', function () {
-    gulp.src('app/stylus/*.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest('app'));
-});
+    browsersync = require('browser-sync');
 
 gulp.task('browsersync', function() {
     browsersync({
@@ -16,8 +9,7 @@ gulp.task('browsersync', function() {
     });
 });
 
-gulp.task('watch', ['browsersync', 'stylus'], function() {
-    gulp.watch('app/stylus/*.styl', ['stylus']);
+gulp.task('watch', ['browsersync'], function() {
     gulp.watch('app/*.html', browsersync.reload);
     gulp.watch('app/*.css', browsersync.reload);
     gulp.watch('app/*.js', browsersync.reload);
